8c2483644b411ed2958d955878512ba3688491d6
JEFF HENRY <henry_jf@hotmail.com>
2016-04-05 15:40:05 -0400

    added vars and math statement

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-var number = function (num1, num2){
-  return num1 + num2;
-
+// var number = function (num1, num2){
+//   return num1 + num2;
+// }
   // return value-one + value-two;
+
+var num1 = document.querySelector('#value-one');
+var num2 = document.querySelector('#value-two');
+var btn = document.querySelector('#calculate-button');
+var sum = document.querySelector('#answer');
+
+function add (num1, num2) {
+  return num1 + num2;
+  // console.log(num1 + num2);
 }
+
+btn.addEventListener('click', function(event) {
+  event.preventDefault();
+  var add = sum.value;
+});
+ console.log(sum);
