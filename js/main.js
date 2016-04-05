f37cf6cad7f7c3953efbc718bd79d38e487a6458
JEFF HENRY <henry_jf@hotmail.com>
2016-04-05 17:13:07 -0400

    js math

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-// var number = function (num1, num2){
-//   return num1 + num2;
-// }
-  // return value-one + value-two;
 
 var num1 = document.querySelector('#value-one');
 var num2 = document.querySelector('#value-two');
 var btn = document.querySelector('#calculate-button');
 var sum = document.querySelector('#answer');
 
-function add (num1, num2) {
-  return num1 + num2;
-  // console.log(num1 + num2);
-}
 
-btn.addEventListener('click', function(event) {
+// function add (num1, num2) {
+//   return num1 + num2;
+//   // console.log(add);
+// }
+
+btn.addEventListener('click', function (event) {
   event.preventDefault();
-  var add = sum.value;
+  // var add = sum.value;
+  var value1 = Number(num1.value);
+  var value2 = Number(num2.value);
+  var value = value1 + value2;
+  // Number(num1.value)= num1;
+  answer.textContent = (value);
 });
- console.log(sum);
+console.log(answer.textContent);
+//  console.log(sum);
