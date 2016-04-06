a19263e81efd9c75dc82711238e51b5ce5b96ce5
JEFF HENRY <henry_jf@hotmail.com>
2016-04-06 07:21:24 -0400

    removed commented out commands

--- js/main.js
+++ js/main.js
@@ -4,20 +4,12 @@ var num2 = document.querySelector('#value-two');
 var btn = document.querySelector('button');
 var sum = document.querySelector('#answer');
 
-
-// function add (num1, num2) {
-//   return num1 + num2;
-//   // console.log(add);
-// }
-
 btn.addEventListener('click', function (event) {
   event.preventDefault();
-  // var add = sum.value;
+
   var value1 = Number(num1.value);
   var value2 = Number(num2.value);
   var value = value1 + value2;
-  // Number(num1.value)= num1;
+
   answer.textContent = (value);
 });
-console.log(answer.textContent);
-//  console.log(sum);
